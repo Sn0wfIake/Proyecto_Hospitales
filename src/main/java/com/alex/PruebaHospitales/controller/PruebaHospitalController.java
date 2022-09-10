49279d423a10104ac99202799e5be74b76b9a458
Alex <alexmatemese@gmail.com>
2022-09-10 18:18:00 +0200

    segundo commit

--- src/main/java/com/alex/PruebaHospitales/controller/PruebaHospitalController.java
+++ src/main/java/com/alex/PruebaHospitales/controller/PruebaHospitalController.java
@@ -1,24 +1,55 @@
 package com.alex.PruebaHospitales.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.PruebaHospitales.datos.DatosHospitales;
-
+import com.alex.PruebaHospitales.model.Hospitales;
 
 @RestController
 public class PruebaHospitalController {
-			
-	@Autowired
-	DatosHospitales datos;
-	
-	
-	@GetMapping("/")
-	  public String home() {
-		datos.insertaHospitales();
-	    return datos.listarHospitales();
-	  }
 
-	
+	@Autowired
+	private DatosHospitales datos;
+
+	@GetMapping("/")
+	public String home() {
+		return "inserta coordenadas";
+	}
+
+	@GetMapping("/lista")
+	public String lista() {
+		datos.insertaHospitales();
+		return datos.listarHospitales();
+	}
+
+	@GetMapping("/ubicacion")
+	public String multiParam(@RequestParam int ejeX, @RequestParam int ejeY) {
+
+		return calculoCercania(ejeX, ejeY);
+
+	}
+
+	private String calculoCercania(int ejeX, int ejeY) {
+
+		if (ejeX < 100 && ejeX > -100 && ejeY > -80 && ejeY < 80) {
+
+			return "es valido";
+		}
+		return "no es valido";
+	}
+
+	private boolean ValidarCoordenada(int ejeX, int ejeY) {
+		if (ejeX < 100 && ejeX > -100 && ejeY > -80 && ejeY < 80) {
+
+			return true;
+		}
+		return false;
+	}
+
 }
