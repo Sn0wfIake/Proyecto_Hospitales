c3624482a56435f216531380977275311db3fc88
Alex <alexmatemese@gmail.com>
2022-09-11 11:42:35 +0200

    codigo acabado

--- src/main/java/com/alex/PruebaHospitales/controller/PruebaHospitalController.java
+++ src/main/java/com/alex/PruebaHospitales/controller/PruebaHospitalController.java
@@ -1,16 +1,11 @@
 package com.alex.PruebaHospitales.controller;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alex.PruebaHospitales.datos.DatosHospitales;
-import com.alex.PruebaHospitales.model.Hospitales;
 
 @RestController
 public class PruebaHospitalController {
@@ -33,29 +28,17 @@ public class PruebaHospitalController {
 	@GetMapping("/ubicacion")
 	public String multiParam(@RequestParam int ejeX, @RequestParam int ejeY) {
 
-		if (!ValidarCoordenada(ejeX, ejeY)) {
+		if (!datos.ValidarCoordenada(ejeX, ejeY)) {
 			return "COORDENADA FUERA DEL INDICE";
 		}
-		String respuesta = "En el caso 1 (contando cuadrados):" + datos.calculoCercaniaEjes(ejeX, ejeY);
-		respuesta += "<br>En el caso 2 (en linea recta): "+ datos.calculoCercaniaRecto(ejeX, ejeY);;
+	
+		String respuesta = "El hospital mas cercano es: "+ datos.calculoCercaniaRecto(ejeX, ejeY);;
 		return respuesta;
 
 	}
-
-	// en el caso de que se muevan en un eje de coordenadas (no puedan ir en linea
-	// recta)
-
 	
 
-	private boolean ValidarCoordenada(int ejeX, int ejeY) {
-		boolean b = false;
-		if (ejeX < 100 && ejeX > -100 && ejeY > -80 && ejeY < 80) {
 
-			b = true;
-		}
-		return b;
-
-	}
 	
 
 	
--- src/main/java/com/alex/PruebaHospitales/datos/DatosHospitales.java
+++ src/main/java/com/alex/PruebaHospitales/datos/DatosHospitales.java
@@ -1,11 +1,8 @@
 package com.alex.PruebaHospitales.datos;
 
-import org.json.simple.*;
 import com.alex.PruebaHospitales.model.Hospitales;
 
 import java.util.ArrayList;
-import java.util.Iterator;
-
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -27,72 +24,9 @@ public class DatosHospitales {
 	public String toString() {
 		return "Hospitales:<br>" + h1 + "<br>" + h2 + "<br>" + h3;
 	}
+
 	
-	public String calculoCercaniaEjes(int ejeX, int ejeY) {
-		int xFinal, yFinal, valorMinimo = 9999, aux;
-		String hospitalCercano = null;
-		listadoHospitales.add(h1);
-		listadoHospitales.add(h2);
-		listadoHospitales.add(h3);
-
-		for (Hospitales h : listadoHospitales) {
-			int xHospital = h.getEjeX();
-			int yHospital = h.getEjeY();
-
-			// manejo X
-			// Si la X del hospital es positiva
-			if (xHospital > 0) {
-				// Si la X del eje es positiva, restamos
-				if (ejeX > 0) {
-					xFinal = xHospital - ejeX;
-				}
-
-			}
-				// comprobamos la otra X y si es positiva, hacemos nuestra suma
-				if (ejeX > 0) {
-					xFinal = xHospital * -1 + ejeX;
-				}
-				// Si la X del HOSPITAL y la de la AMBULANCIA SON negativas, convertimos una y sumamos
-				xFinal = xHospital + ejeX * -1;
-
-				
-				// manejo Y
-				// Si la Y del hospital es positiva
-				if (yHospital > 0) {
-					// Si la Y del eje es positiva, restamos
-					if (ejeY > 0) {
-						yFinal = yHospital - ejeY;
-					}
-
-				}
-					// comprobamos la otra Y y si es positiva, hacemos nuestra suma
-					if (ejeY > 0) {
-						yFinal = yHospital * -1 + ejeY;
-					}
-					// Si la Y del HOSPITAL y la de la AMBULANCIA SON negativas, convertimos una y sumamos
-					yFinal = yHospital + ejeY * -1;
-
-					if (xFinal<0) {
-						xFinal=xFinal*-1;
-					}
-					if (yFinal<0) {
-						yFinal=yFinal*-1;
-					}
-					//Buscamos el minimo
-					aux=yFinal+xFinal;
-					System.out.println("ESTO ES EL FOR");
-					if (aux<=valorMinimo) {
-						System.out.println("PASO POR AQUI");
-						hospitalCercano = h.toString();
-						valorMinimo= aux;
-						
-					}
-					
-			}
-		
-		return "el hospital mas cercano es: "+ hospitalCercano;
-
-	}
+	
 
 	public String calculoCercaniaRecto(int ejeX, int ejeY) {
 		listadoHospitales.add(h1);
@@ -103,13 +37,22 @@ public class DatosHospitales {
 		for (Hospitales h : listadoHospitales) {
 			int aux=((ejeX-h.getEjeX())*(ejeX-h.getEjeX())+(ejeY-h.getEjeY())*(ejeY-h.getEjeY()));
 			if (aux<=distancia) {
-				System.out.println("PASO POR AQUI");
 				 hospitalCercano = h.toString();
 				distancia= aux;
 				
 			}
 		}
 		return hospitalCercano;
+	}
+	
+	public boolean ValidarCoordenada(int ejeX, int ejeY) {
+	
+		if (ejeX < 100 && ejeX > -100 && ejeY > -80 && ejeY < 80) {
+
+			return true;
+		}
+		return false;
+		
 	}
 	
 }
